22007630e27c34893d96f4f3b725e8762ca38c45
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2018-02-01 15:31:01 -0500

    finished to exericse 10

--- lab1.ml
+++ lab1.ml
@@ -99,7 +99,7 @@ appropriate OCaml expression to assign the value to the variable
 exercise1 below.
 ......................................................................*)
 
-let exercise3 () = failwith "exercise3 not implemented" ;;
+let exercise3 () = -(5-3) ;;
 
 (* Hint: The OCaml concrete expression
 
@@ -124,24 +124,20 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
+let exercise5a : int = 42 ;;
 
-let exercise5a : ??? = 42 ;;
-
-let exercise5b : ??? =
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : (int * float) -> int  =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : int -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : bool -> bool list =
   fun x -> if x then [x] else [] ;;
-
-remove this end of comment line too ----> *)
 
 (*======================================================================
 Part 3: First-order functional programming
@@ -170,10 +166,12 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  [] -> []
+  | h::t -> h*h::square_all(t) ;;
 
-let exercise6 = [] ;;
+let exercise6 = [1;3;6] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
